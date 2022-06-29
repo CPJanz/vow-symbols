8ea61d6a11aed91c96b93b9ddfc8652df0791cd8
CPJanz <carl.janz@gmail.com>
2022-06-29 00:07:39 -0700

    Removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import styled from "styled-components";
 import Symbols from "./Symbols";
-import { randomInt } from "crypto";
 
 const SymbolGrid = styled.div`
   display: grid;
